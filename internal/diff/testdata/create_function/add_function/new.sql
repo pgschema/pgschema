3569db6d4460a87bcd8e62509053a705c468efce
tianzhou <t@bytebase.com>
2025-06-23 15:28:21 +0800

    feat: CREATE FUNCTION

---
+++ internal/diff/testdata/create_function/add_function/new.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION process_order(
+    order_id integer,
+    discount_percent numeric DEFAULT 0
+)
+RETURNS numeric
+LANGUAGE plpgsql
+SECURITY DEFINER
+VOLATILE
+AS $$
+DECLARE
+    total numeric;
+BEGIN
+    SELECT amount INTO total FROM orders WHERE id = order_id;
+    RETURN total - (total * discount_percent / 100);
+END;
+$$;
