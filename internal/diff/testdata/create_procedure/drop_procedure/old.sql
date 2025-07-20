1e1b3e17083feb62c4f473138efc743a608475e5
Tianzhou <t@bytebase.com>
2025-07-20 23:23:27 +0800

    chore: fix syntax error

--- internal/diff/testdata/create_procedure/drop_procedure/old.sql
+++ internal/diff/testdata/create_procedure/drop_procedure/old.sql
@@ -5,10 +5,10 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     DELETE FROM audit_logs 
-    WHERE created_at < NOW() - INTERVAL '%s days' USING days_old;
+    WHERE created_at < NOW() - INTERVAL '1 day' * days_old;
     
     DELETE FROM temp_data 
-    WHERE created_at < NOW() - INTERVAL '%s days' USING days_old;
+    WHERE created_at < NOW() - INTERVAL '1 day' * days_old;
     
     COMMIT;
 END;
