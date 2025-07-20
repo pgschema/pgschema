c7df779737c5c38a110f290b909cc6fb1b99e413
Tianzhou <t@bytebase.com>
2025-07-20 15:37:17 +0800

    chore: adjust dump view indentation

--- testdata/employee/pgschema.sql
+++ testdata/employee/pgschema.sql
@@ -247,11 +247,11 @@ CREATE TABLE title (
 --
 
 CREATE VIEW dept_emp_latest_date AS
- SELECT emp_no,
+SELECT 
+    emp_no,
     max(from_date) AS from_date,
     max(to_date) AS to_date
-   FROM dept_emp
-  GROUP BY emp_no;
+FROM dept_emp GROUP BY emp_no;
 
 
 --
@@ -259,9 +259,9 @@ CREATE VIEW dept_emp_latest_date AS
 --
 
 CREATE VIEW current_dept_emp AS
- SELECT l.emp_no,
+SELECT 
+    l.emp_no,
     d.dept_no,
     l.from_date,
     l.to_date
-   FROM (dept_emp d
-     JOIN dept_emp_latest_date l ON (((d.emp_no = l.emp_no) AND (d.from_date = l.from_date) AND (l.to_date = d.to_date))));
+FROM (dept_emp d JOIN dept_emp_latest_date l ON (((d.emp_no = l.emp_no) AND (d.from_date = l.from_date) AND (l.to_date = d.to_date))));
