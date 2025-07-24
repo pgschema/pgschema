c213d2ccfab134dd93422bb715c1bb3b10654c6e
Tianzhou <t@bytebase.com>
2025-07-24 22:53:26 +0800

    chore: add_uk test case

--- testdata/diff/create_table/add_uk/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-ADD CONSTRAINT users_email_key UNIQUE (email);
-
-ALTER TABLE users
-ADD CONSTRAINT users_username_key UNIQUE (username);
--- testdata/diff/create_table/add_uk/new.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    username text NOT NULL UNIQUE,
-    email text NOT NULL UNIQUE
-);
