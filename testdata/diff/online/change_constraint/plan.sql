3d672f160b12a265de25b7c45ed568f48c15f689
Tianzhou <t@bytebase.com>
2025-08-18 23:39:10 +0800

    feat: adjust online test case

--- testdata/diff/online/change_constraint/plan.sql
+++ testdata/diff/online/change_constraint/plan.sql
@@ -1,4 +1,6 @@
 ALTER TABLE orders DROP CONSTRAINT check_amount_positive;
 
 ALTER TABLE orders
-ADD CONSTRAINT check_amount_positive CHECK (amount > 0);
+ADD CONSTRAINT check_amount_positive CHECK (amount > 0) NOT VALID;
+
+ALTER TABLE orders VALIDATE CONSTRAINT check_amount_positive;
