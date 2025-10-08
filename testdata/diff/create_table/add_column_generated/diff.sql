e17354c1de586c84bbf84618faadfc27ebd58b0e
Tianzhou <t@bytebase.com>
2025-10-09 00:08:19 +0800

    fix: generated column PK and NULL handling

--- testdata/diff/create_table/add_column_generated/diff.sql
+++ testdata/diff/create_table/add_column_generated/diff.sql
@@ -1,1 +1,3 @@
-ALTER TABLE merge_request ADD COLUMN iid integer NOT NULL GENERATED ALWAYS AS (CAST(data ->> 'iid' AS int)) STORED;
+ALTER TABLE merge_request
+ADD COLUMN iid integer PRIMARY KEY GENERATED ALWAYS AS (CAST(data ->> 'iid' AS int)) STORED;
+ALTER TABLE merge_request ADD COLUMN title text GENERATED ALWAYS AS (data ->> 'title') STORED;
