d481daad31535359b48ee6b618cde3d361cbf691
Tianzhou <t@bytebase.com>
2025-11-23 05:47:07 -0800

    chore: remove unused file

--- testdata/diff/create_table/add_table_like_forward_ref/error_case.sql
+++
@@ -1,6 +0,0 @@
--- Test error case: LIKE references non-existent table
-CREATE TABLE public.orders (
-    id SERIAL PRIMARY KEY,
-    order_date DATE NOT NULL,
-    LIKE public.nonexistent_table INCLUDING DEFAULTS
-);
