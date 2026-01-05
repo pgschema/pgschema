70dfd766596d7b524d3b3c855889c19b28f7276a
Tianzhou <t@bytebase.com>
2026-01-04 20:18:38 -0800

    chore: adjust policy table reference test case (#228)

--- testdata/diff/create_policy/add_policy/diff.sql
+++ testdata/diff/create_policy/add_policy/diff.sql
@@ -1,3 +1,5 @@
+ALTER TABLE orders ENABLE ROW LEVEL SECURITY;
+CREATE POLICY orders_user_access ON orders FOR SELECT TO PUBLIC USING (user_id IN ( SELECT users.id FROM users));
 CREATE POLICY "UserPolicy" ON users TO PUBLIC USING (tenant_id = current_setting('app.current_tenant')::integer);
 CREATE POLICY "my-policy" ON users FOR INSERT TO PUBLIC WITH CHECK ((role)::text = 'user');
 CREATE POLICY "select" ON users FOR SELECT TO PUBLIC USING (true);
